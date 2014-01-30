a6aae8377f157e0fee220b255529366e8a123dce
Manuel Navarro <manuelrubydev@gmail.com>
2014-01-30 20:42:22 +0000

    mccs project fist commit: only rails 4 + postgresql - test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mccs::Application.config.secret_key_base = 'fd44307e276a2caf963b6b78c5710366b7b7a3a672cc711492884f45adccbe03c757c07457c5d068a3065d630233e5c058cc16b61e7be6a59a5853499036725f'
